2be80a8c8d90451a1bf3961cdba5c3da904ec47b
Kishan Patel <kpatel@savanacorp.net>
2021-10-27 14:21:27 -0400

    adding subnet

---
+++ subnets.tf
@@ -0,0 +1,19 @@
+#public subnet
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = var.public-subnet-cidr
+
+  tags = {
+    Name = "${var.public-subnet-tag}-${var.env}"
+  }
+}
+
+#private subnet
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.terraform_vpc.id
+  cidr_block = var.private-subnet-cidr
+
+  tags = {
+    Name = "${var.private-subnet-tag}-${var.env}-"
+  }
+}
--- vars.tf
+++ vars.tf
@@ -14,3 +14,19 @@ variable "availabilityZone" {
 variable "vpc-tag" {
   default = "tfvpc"
 }
+
+#public subnet
+variable "public-subnet-cidr"{  
+  default = "192.168.1.0/24"
+}
+variable "public-subnet-tag" {
+  default= "tf-public"
+}
+
+#private subnet
+variable "private-subnet-cidr" {  
+  default = "192.168.2.0/24"
+}
+variable "private-subnet-tag" {
+  default= "tf-private"
+}
